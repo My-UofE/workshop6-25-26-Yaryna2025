ca9aec7a5dd5fe3b5b0c9c20384d7b7ddbb73991
Yaryna Matselyukh <ym449@exeter.ac.uk>
2026-02-26 08:51:00 +0000

    Completed 1.2 Call stack and exception propagation

---
+++ Unchecked2App.java
@@ -0,0 +1,118 @@
+// public class Unchecked2App {
+
+//     public static void main(String args[]) {
+//         System.out.println("### Enter main() ...");
+
+//         String s1 = null;
+
+//         if (args.length > 0) {
+//             s1 = args[0];
+//         }
+
+//         methodA(s1);
+
+//         System.out.println("### Exit main()!");
+//     }
+
+//     static void methodA(String s1) {
+//         System.out.println("### Enter methodA() ...");
+//         methodB(s1);
+//         System.out.println("### Exit methodA()!");
+//     }
+
+//     static void methodB(String s1) {
+//         System.out.println("### Enter methodB() ...");
+
+//         try {
+//             String s2 = s1.toUpperCase();
+//             System.out.println("s1: " + s1);
+//             System.out.println("s2: " + s2);
+//         } catch (NullPointerException e) {
+//             System.out.println("Exception handled in methodB()");
+//         }
+
+//         System.out.println("### Exit methodB()!");
+//     }
+// }
+
+
+
+
+
+
+// public class Unchecked2App {
+
+//     public static void main(String args[]) {
+//         System.out.println("### Enter main() ...");
+
+//         String s1 = null;
+
+//         if (args.length > 0) {
+//             s1 = args[0];
+//         }
+
+//         methodA(s1);
+
+//         System.out.println("### Exit main()!");
+//     }
+
+//     static void methodA(String s1) {
+//         System.out.println("### Enter methodA() ...");
+
+//         try {
+//             methodB(s1);
+//         } catch (NullPointerException e) {
+//             System.out.println("Exception handled in methodA()");
+//         }
+
+//         System.out.println("### Exit methodA()!");
+//     }
+
+//     static void methodB(String s1) {
+//         System.out.println("### Enter methodB() ...");
+//         String s2 = s1.toUpperCase();
+//         System.out.println("s1: " + s1);
+//         System.out.println("s2: " + s2);
+//         System.out.println("### Exit methodB()!");
+//     }
+// }
+
+
+
+
+
+
+public class Unchecked2App {
+
+    public static void main(String args[]) {
+        System.out.println("### Enter main() ...");
+
+        String s1 = null;
+
+        if (args.length > 0) {
+            s1 = args[0];
+        }
+
+        try {
+            methodA(s1);
+        } catch (NullPointerException e) {
+            System.out.println("Exception handled in main()");
+        }
+
+        System.out.println("### Exit main()!");
+    }
+
+    static void methodA(String s1) {
+        System.out.println("### Enter methodA() ...");
+        methodB(s1);
+        System.out.println("### Exit methodA()!");
+    }
+
+    static void methodB(String s1) {
+        System.out.println("### Enter methodB() ...");
+        String s2 = s1.toUpperCase();
+        System.out.println("s1: " + s1);
+        System.out.println("s2: " + s2);
+        System.out.println("### Exit methodB()!");
+    }
+}
